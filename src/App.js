a801da57ac08e91c7f85df5f79031eeefb0c7b68
rafif gunawan <rafifgunawan88@gmail.com>
2023-02-09 13:17:26 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./TodoList.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const response = await fetch("http://localhost:8080/todos");
+    const data = await response.json();
+    setTodos(data);
+  }
+
+
+  const handleInputChange = event => {
+    setInputValue(event.target.value);
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const data = {text: inputValue, completed: false};
+
+    await fetch("http://localhost:8080/todos",{
+       method : "POST",
+       body: JSON.stringify(data),
+       headers:{
+        'content-type' : 'application/json'
+       }
+    })
+    getData();
+    setInputValue("");
+  };
+
+  const handleTodoClick = todoId => {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === todoId) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+        return todo;
+      })
+    );
+  };
+
+  const handleDeleteClick = async (todoId) => {
+    const data = {text: inputValue, completed: false};
+
+    await fetch(`http://localhost:8080/todos/${todoId}`,{
+       method : "DELETE",
+       body: JSON.stringify(data),
+       headers:{
+        'content-type' : 'application/json'
+       }
+    })
+
+    getData();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-list-container">
+      <h1 className="todo-list-title">To Do List</h1>
+      <form onSubmit={handleFormSubmit} className="todo-list-form">
+        <input type="text" value={inputValue} onChange={handleInputChange} className="todo-list-input" />
+        <button type="submit" className="todo-list-button">Tambah Tugas</button>
+      </form>
+      <ul className="todo-list">
+        {todos.map(todo => (
+          <li key={todo.id} className={`todo-list-item ${todo.completed ? "completed" : ""}`}>
+            {todo.text}
+            <button onClick={() => handleTodoClick(todo.id)} className="todo-list-toggle-button">{todo.completed ? "Uncompleted" : "Completed"}</button>
+            <button onClick={() => handleDeleteClick(todo.id)} className="todo-list-delete-button">Hapus</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
